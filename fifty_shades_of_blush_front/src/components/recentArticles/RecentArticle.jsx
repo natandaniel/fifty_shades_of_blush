0522b5395efb22798757f644d8d148ad281f4be6
natan.daniel <natan.daniel0@gmail.com>
2019-07-24 22:28:16 +0200

    rendering date

--- fifty_shades_of_blush_front/src/components/recentArticles/RecentArticle.jsx
+++ fifty_shades_of_blush_front/src/components/recentArticles/RecentArticle.jsx
@@ -10,37 +10,42 @@ import '../../assets/css/recentArticle.css';
 
 class RecentArticle extends React.Component {
 
-    constructor(props) {
-        super(props);
-      }
+  constructor(props) {
+    super(props);
+  }
 
   render() {
     return (
-        <Card className="card" key={this.props.article.entity._links.self.href}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              alt={this.props.article.entity.type}
-              height="200"
-              image={require(`../../assets/img/${this.props.article.entity.imgName}.jpg`)}
-              title={this.props.article.entity.title}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
+      <Card className="card" key={this.props.article.entity._links.self.href}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt={this.props.article.entity.type}
+            height="200"
+            image={require(`../../assets/img/${this.props.article.entity.imgName}.jpg`)}
+            title={this.props.article.entity.title}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
               {this.props.article.entity.title}
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
+            </Typography>
+            <Typography variant="subtitle2" color="textSecondary" component="p">
               {this.props.article.entity.subtitle}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              Read more
+            </Typography>
+            <Typography component="h6">{new Intl.DateTimeFormat('en-US', {
+              year: 'numeric',
+              month: 'long',
+              day: '2-digit'
+            }).format(new Date(this.props.article.entity.createdAt))}</Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Read more
             </Button>
-          </CardActions>
-        </Card>
-      );
+        </CardActions>
+      </Card>
+    );
   }
 
 }
