a4d06ecf9ee13329954b6ddc7bfb5ea296552cfe
natan.daniel <natan.daniel0@gmail.com>
2019-07-27 13:08:46 +0200

    updating latest article component design

--- fifty_shades_of_blush_front/src/components/recentArticles/LatestArticle.jsx
+++ fifty_shades_of_blush_front/src/components/recentArticles/LatestArticle.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import '../../assets/css/components/latestArticle.css';
@@ -14,29 +15,38 @@ class LatestArticle extends React.Component {
     const latest = this.props.latestArticle.map(article =>
       <div key={article.entity._links.self.href}>
         <Paper className="root" key={article.entity._links.self.href}>
-          <div className="header">
-            <img className="img" src={require(`../../assets/img/${article.entity.imgName}.jpg`)} width="55%" alt="banner" />
-            <div className="articleInfo">
-              <Typography variant="h5" component="h2">
-                {article.entity.title}
-              </Typography>
-              <Typography variant="subtitle2" color="textSecondary" component="p">
-                {article.entity.subtitle}
-              </Typography>
-              <Typography component="h6">{new Intl.DateTimeFormat('en-US', {
-                year: 'numeric',
-                month: 'long',
-                day: '2-digit'
-              }).format(new Date(article.entity.createdAt))}</Typography>
-            </div>
-          </div>
-          <div>
-            <Typography>{article.entity.body} </Typography>
-          </div>
-
-        </Paper></div>);
-
-    return (<div>{latest}</div>);
+          <Grid container spacing={2}>
+            <Grid item md={5} xs={12}>
+              <div className="articleInfo">
+                <Typography variant="h5" component="h2">
+                  {article.entity.title}
+                </Typography>
+                <Typography variant="subtitle2" color="textSecondary" component="p">
+                  {article.entity.subtitle}
+                </Typography>
+                <Typography component="h6">{new Intl.DateTimeFormat('en-US', {
+                  year: 'numeric',
+                  month: 'long',
+                  day: '2-digit'
+                }).format(new Date(article.entity.createdAt))}</Typography>
+              </div>
+            </Grid>
+            <Grid item md={7} xs={12}>
+              <img className="img" src={require(`../../assets/img/${article.entity.imgName}.jpg`)} width="55%" alt="article" />
+            </Grid>
+            <Grid item >
+              <div className="article">
+                <Typography>{article.entity.body}</Typography>
+              </div>
+            </Grid>
+          </Grid>
+        </Paper>
+      </div>);
+    return (
+      <div>
+        {latest}
+      </div>
+    );
   }
 }
 
