23a5e01b3626e684bb51edf2f35e2e890b891d69
natan.daniel <natan.daniel0@gmail.com>
2019-08-17 10:48:38 +0200

    updating article creation dialog

--- fifty_shades_of_blush_front/src/components/article/CreateArticle.jsx
+++ fifty_shades_of_blush_front/src/components/article/CreateArticle.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+import '../../assets/css/components/article/createArticle.css'
+
 const client = require('../../tools/rest/client');
 const follow = require('../../tools/rest/follow');
 const root = 'http://localhost:8080/api';
+
+const categories = [
+	'beauty',
+	'fashion',
+	'travel',
+	'lifestyle'
+];
 
 class CreateArticle extends React.Component {
 
 	constructor(props) {
 		super(props);
-		this.handleSubmit = this.handleSubmit.bind(this);
-		this.onCreate = this.onCreate.bind(this);
+		this.state = {}
 	}
 
 	onCreate(newArticle) {
@@ -24,7 +36,7 @@ class CreateArticle extends React.Component {
 		})
 	}
 
-	handleSubmit(e) {
+	handleSubmit = (e) => {
 		e.preventDefault();
 		const newArticle = {};
 		this.props.attributes.forEach(attribute => {
@@ -41,28 +53,59 @@ class CreateArticle extends React.Component {
 
 	render() {
 
-		const inputs = this.props.attributes.map(attribute =>
-			<p key={attribute}>
-				<input type="text" placeholder={attribute} ref={attribute} className="field" />
-			</p>
-		);
-
 		return (
 			<div>
-				<a href="#createArticle">Create</a>
 
-				<div id="createArticle" className="modalDialog">
-					<div>
-						<a href="#" title="Close" className="close">X</a>
+				<form onSubmit={this.handleSubmit}>
+					<TextField
+						id="title"
+						label="Article Title"
+						placeholder="Write title here ..."
+						className="articleTitle"
+						margin="normal"
+					/>
+					<TextField
+						id="subtitle"
+						label="Article Subtitle"
+						placeholder="Write subtitle here ..."
+						className="articleSubtitle"
+						margin="normal"
+					/>
+					<TextField
+						id="category"
+						select
+						className="articleCategory"
+						SelectProps={{
+							native: true,
+						}}
+						helperText="Choose article category"
+						margin="normal"
+					>
+						{categories.map(option => (
+							<option key={option} value={option}>
+								{option}
+							</option>
+						))}
+					</TextField>
+					<TextField
+						id="standard-textarea"
+						label="Article Body"
+						placeholder="Write article here ..."
+						multiline
+						className="articleBody"
+						margin="normal"
+					/>
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						color="primary"
+						className="submit"
+					>
+						Submit
+            		</Button>
+				</form>
 
-						<h2>Create new article</h2>
-
-						<form>
-							{inputs}
-							<button onClick={this.handleSubmit}>Create</button>
-						</form>
-					</div>
-				</div>
 			</div>
 		)
 	}
