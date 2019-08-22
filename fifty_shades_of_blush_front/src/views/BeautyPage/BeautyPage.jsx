df37ba9b40daaeb3b1b92948bb25d384f05b5e5f
natandaniel <natan.daniel0@gmail.com>
2019-08-22 17:48:30 +0200

    refactoring with ArticlesService

--- fifty_shades_of_blush_front/src/views/BeautyPage/BeautyPage.jsx
+++ fifty_shades_of_blush_front/src/views/BeautyPage/BeautyPage.jsx
@@ -1,108 +1,68 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
+import axios from 'axios';
 
+import Container from '@material-ui/core/Container';
 import Header from '../../components/header/Header.jsx'
 import Footer from '../../components/footer/Footer.jsx'
 import ArticleCardGrid from '../../components/article/ArticleCardGrid.jsx';
 import Article from '../../components/article/Article.jsx';
 
+import ArticlesService from '../../tools/dataProvider/ArticlesService';
 
 const when = require('when');
-const client = require('../../tools/rest/client');
-const follow = require('../../tools/rest/follow');
-const root = 'http://localhost:8080/api';
 
 class BeautyPage extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { beautyArticles: [], displayedArticleKey: "", displayedArticle: [], displayedArticleParagraphs: [] };
-    this.getBeautyArticles = this.getBeautyArticles.bind(this);
-    this.getLatestBeautyArticleWithParagraphs = this.getLatestBeautyArticleWithParagraphs.bind(this);
-    this.updateDisplayedArticle = this.updateDisplayedArticle.bind(this);
+    this.state = {
+      beautyArticles: [],
+      displayedArticle: []
+    };
   }
 
   loadFromServer() {
-
-    follow(client, root, [
-      { rel: 'articles', params: {} }]
-    ).then(articleCollection => {
-
-      this.setState({
-        displayedArticleKey: articleCollection.entity._links.latestBeauty.href
-      });
-
-      this.getBeautyArticles(articleCollection.entity._links.beauty.href);
-      this.getLatestBeautyArticleWithParagraphs(articleCollection.entity._links.latestBeauty.href);
-    });
+    this.getBeautyArticles()
+    this.getLatestBeautyArticle()
   }
 
-  getBeautyArticles(beautyUri) {
-    client({
-      method: 'GET',
-      path: beautyUri
-    }).then(beautyArticleCollection => {
-      return beautyArticleCollection.entity._embedded.articleResources.map(beautyArticle =>
-        client({
-          method: 'GET',
-          path: beautyArticle._links.self.href
-        })
-      );
-    }).then(beautyArticlePromises => {
-      return when.all(beautyArticlePromises);
-    }).done(beautyArticles => {
-      this.setState({
-        beautyArticles: beautyArticles
-      });
+  getLatestBeautyArticle() {
+    ArticlesService.getLatestBeautyArticleEntity().then(latestArticleEntity => {
+      if (latestArticleEntity.data._embedded) {
+        return latestArticleEntity.data._embedded.articleResources.map(article =>
+          axios.get(article._links.self.href)
+        )
+      } else {
+        return new Array()
+      }
+    }).then(articlePromises => {
+      return when.all(articlePromises)
+    }).then(latestArticleArray => {
+      this.setState({ displayedArticle: latestArticleArray })
     })
   }
 
-  getLatestBeautyArticleWithParagraphs(latestBeautyUri) {
-    client({
-      method: 'GET',
-      path: latestBeautyUri
-    }).then(latestBeautyArticleCollection => {
-      return latestBeautyArticleCollection.entity._embedded.articleResources.map(beautyArticle =>
-        client({
-          method: 'GET',
-          path: beautyArticle._links.self.href
-        })
-      );
-    }).then(latestBeautyArticlePromise => {
-      return when.all(latestBeautyArticlePromise);
-    }).done(displayedArticle => {
-
+  getBeautyArticles() {
+    ArticlesService.getBeautyArticlesEntity().then(beautyArticlesEntity => {
+      if (beautyArticlesEntity.data._embedded) {
+        return beautyArticlesEntity.data._embedded.articleResources.map(article =>
+          axios.get(article._links.self.href)
+        )
+      } else {
+        return new Array()
+      }
+    }).then(articlePromises => {
+      return when.all(articlePromises)
+    }).then(beautyArticlesEntityArray => {
       this.setState({
-        displayedArticle: displayedArticle
-      });
-
-      this.state.displayedArticle.map(displayedArticle =>
-
-        client({
-          method: 'GET',
-          path: displayedArticle.entity._links.paragraphs.href
-        }).then(result => {
-          return result.entity._embedded.articleContents.map(articleContent =>
-            client({
-              method: 'GET',
-              path: articleContent._links.self.href
-            })
-          );
-        }).then(articleContentPromises => {
-          return when.all(articleContentPromises);
-        }).done(paragraphs => {
-          this.setState({
-            displayedArticleParagraphs: paragraphs
-          });
-        }));;
+        beautyArticles: beautyArticlesEntityArray
+      })
     })
   }
 
-  updateDisplayedArticle(displayedArticleKey, displayedArticle, displayedArticleParagraphs) {
+  updateDisplayedArticle = (displayedArticle) => {
     this.setState({
-      displayedArticleKey: displayedArticleKey,
       displayedArticle: displayedArticle,
-      displayedArticleParagraphs: displayedArticleParagraphs
     });
   }
 
@@ -113,12 +73,12 @@ class BeautyPage extends React.Component {
   render() {
 
     return (
-        <Container className="mainContainer" maxWidth="lg">
-          <Header />
-          <Article id="displayedArticle" key={this.state.displayedArticleKey} article={this.state.displayedArticle} articleParagraphs={this.state.displayedArticleParagraphs} />
-          <ArticleCardGrid recentArticles={this.state.beautyArticles} displayedArticleHandler={this.updateDisplayedArticle} />
-          <Footer />
-        </Container>
+      <Container className="mainContainer" maxWidth="lg">
+        <Header />
+        <Article key="displayedArticle" article={this.state.displayedArticle} />
+        <ArticleCardGrid recentArticles={this.state.beautyArticles} displayedArticleHandler={this.updateDisplayedArticle} />
+        <Footer />
+      </Container>
     );
   }
 }
