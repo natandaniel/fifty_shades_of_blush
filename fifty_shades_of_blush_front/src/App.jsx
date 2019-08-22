43a7a1bf83ef31fcb085c5bfe7b5f0d1e1eb4585
natan.daniel <natan.daniel0@gmail.com>
2019-08-22 23:32:49 +0200

    update private route'

--- fifty_shades_of_blush_front/src/App.jsx
+++ fifty_shades_of_blush_front/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Router, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import LoginPage from './views/LoginPage/LoginPage.jsx';
 import LandingPage from './views/LandingPage/LandingPage.jsx';
 import BFTLPage from './views/BFTLPage/BFTLPage.jsx';
+import CreateArticle from './components/article/CreateArticle.jsx';
 
 var hist = createBrowserHistory();
 
@@ -22,15 +23,27 @@ class App extends React.Component {
             <Router history={hist}>
                 <Switch>
                     <Route exact path="/" component={LandingPage} />
-                    <Route exact path="/login" component={LoginPage} />
                     <Route path="/beauty" render={(props) => <BFTLPage {...props} page={"beauty"} />} />
                     <Route path="/fashion" render={(props) => <BFTLPage {...props} page={"fashion"} />} />
                     <Route path="/travel" render={(props) => <BFTLPage {...props} page={"travel"} />} />
                     <Route path="/lifestyle" render={(props) => <BFTLPage {...props} page={"lifestyle"} />} />
+                    <Route exact path="/login" component={LoginPage} />
+                    <PrivateRoute exact path="/admin/create" component={CreateArticle} />
                 </Switch>
             </Router>
         );
     }
 }
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={(props) => (
+        sessionStorage.getItem('isAuth') === 'true'
+            ? <Component {...props} />
+            : <Redirect to={{
+                pathname: '/login',
+                state: { from: props.location }
+            }} />
+    )} />
+)
 
 export default App;
