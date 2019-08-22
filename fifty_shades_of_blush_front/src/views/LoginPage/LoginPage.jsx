c8bfddf0613092f275dd9f7cbb31cabcfe22d38e
natan.daniel <natan.daniel0@gmail.com>
2019-08-22 23:35:36 +0200

    removing unused handler + setting isAuth session item upon login + redirecting to previous route

--- fifty_shades_of_blush_front/src/views/LoginPage/LoginPage.jsx
+++ fifty_shades_of_blush_front/src/views/LoginPage/LoginPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Redirect} from "react-router-dom";
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -8,18 +9,14 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import AuthenticationService from '../../tools/authProvider/AuthenticationService';
 
-
-const client = require('../../tools/rest/client');
-const API_URL = 'http://localhost:8080/api';
-
 class LoginPage extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      hasLoginFailed: false,
       username: "",
-      password: ""
+      password: "",
+      redirectToReferrer: false
     };
   }
 
@@ -32,49 +29,32 @@ class LoginPage extends React.Component {
     )
   }
 
-  handleSubmit = event => {
+  login = event => {
     event.preventDefault();
-
-    client({
-      method: 'POST',
-      path: `${API_URL}/authenticate`,
-      entity: { "username": this.state.username, "password": this.state.password },
-      headers: { 'Content-Type': 'application/json' }
-    }).done(response => {
-
-      if (response.status < 200 || response.status >= 300) {
-        throw new Error(response.statusText);
-      } else {
-        sessionStorage.setItem('isLoggedIn', 'true');
-        sessionStorage.setItem('token', response.entity.token);
-        this.props.history.push('/')
-      }
-    });
-  }
-
-  handleSubmit2 = event => {
-    event.preventDefault();
-
     AuthenticationService.executeBasicAuthenticationService(this.state.username, this.state.password)
-      .then(() => {
-        sessionStorage.setItem('isLoggedIn', 'true');
+      .then((response) => {
+        console.log(response)
+        sessionStorage.setItem('isAuth', 'true');
         AuthenticationService.registerSuccessfulLogin(this.state.username, this.state.password)
-        this.props.history.push('/')
-      }).catch(() => {
-        this.setState({ hasLoginFailed: true })
+        this.setState({redirectToReferrer: true});
+      }).catch((exc) => {
+        console.log(exc)
       })
   }
 
   render() {
 
-    if (sessionStorage.getItem('isLoggedIn') === 'true') {
-      this.props.history.push('/');
+    const { from } = this.props.location.state || { from: { pathname: '/' } }
+
+    console.log(from)
+
+    if (sessionStorage.getItem('isAuth') === 'true') {
+      return <Redirect to={from} />
     }
 
     return (
       <Container component="main" maxWidth="xs">
         <CssBaseline />
-        {this.state.hasLoginFailed && <div className="alert alert-warning">Invalid Credentials</div>}
         <div className="paper">
           <Avatar className="avatar">
             <LockOutlinedIcon />
@@ -82,7 +62,7 @@ class LoginPage extends React.Component {
           <Typography component="h1" variant="h5">
             ADMIN ACCESS
           </Typography>
-          <form className="form" noValidate onSubmit={this.handleSubmit2}>
+          <form className="form" noValidate onSubmit={this.login}>
             <TextField
               variant="outlined"
               margin="normal"
