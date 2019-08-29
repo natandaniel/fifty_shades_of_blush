33d8b887e07b4d98620e0007d2d4c82b96668eb2
natandaniel <natan.daniel0@gmail.com>
2019-08-29 16:56:33 +0200

    initializing servlet context

--- fsob-back/fsob/src/main/java/fifty/shades/of/blush/FiftyShadesOfBlush.java
+++ fsob-back/fsob/src/main/java/fifty/shades/of/blush/FiftyShadesOfBlush.java
@@ -2,9 +2,10 @@ package fifty.shades.of.blush;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FiftyShadesOfBlush {
+public class FiftyShadesOfBlush extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(FiftyShadesOfBlush.class, args);
