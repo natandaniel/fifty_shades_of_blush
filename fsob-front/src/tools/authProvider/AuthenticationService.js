bfc575e6ff04381d8243144a41515e80cd02135e
natandaniel <natan.daniel0@gmail.com>
2019-08-29 17:38:57 +0200

    updating api url

--- fsob-front/src/tools/authProvider/AuthenticationService.js
+++ fsob-front/src/tools/authProvider/AuthenticationService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api';
+const API_URL = 'http://localhost:8080/fsob/api';
 
 export const USER_NAME_SESSION_ATTRIBUTE_NAME = 'authenticatedUser'
 
--- fsob-front/src/tools/dataProvider/ArticlesService.js
+++ fsob-front/src/tools/dataProvider/ArticlesService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api';
+const API_URL = 'http://localhost:8080/fsob/api';
 
 class ArticlesService {
 
