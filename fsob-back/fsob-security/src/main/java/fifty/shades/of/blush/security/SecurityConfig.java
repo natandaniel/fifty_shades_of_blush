dd5b6d92abfea9ac8b6ef2b7efa3792e33fb2f58
natandaniel <natan.daniel0@gmail.com>
2019-09-04 10:36:40 +0200

    setting x-frame-options to same origin

--- fsob-back/fsob-security/src/main/java/fifty/shades/of/blush/security/SecurityConfig.java
+++ fsob-back/fsob-security/src/main/java/fifty/shades/of/blush/security/SecurityConfig.java
@@ -66,6 +66,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+        
+        	.headers().frameOptions().sameOrigin()
+        	
+        	.and()
+        	
 	        .cors()
 	        
 	        .and()
