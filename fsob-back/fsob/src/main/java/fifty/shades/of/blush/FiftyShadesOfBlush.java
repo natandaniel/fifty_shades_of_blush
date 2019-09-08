6e32ace69a5850d43e053f8f7e19c2deaf78c7c4
natan.daniel <natan.daniel0@gmail.com>
2019-09-08 23:20:27 +0200

    file system image download

--- fsob-back/fsob/src/main/java/fifty/shades/of/blush/FiftyShadesOfBlush.java
+++ fsob-back/fsob/src/main/java/fifty/shades/of/blush/FiftyShadesOfBlush.java
@@ -2,9 +2,15 @@ package fifty.shades.of.blush;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import fifty.shades.of.blush.data.properties.FileStorageProperties;
+
 @SpringBootApplication
+@EnableConfigurationProperties({
+    FileStorageProperties.class
+})
 public class FiftyShadesOfBlush extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
